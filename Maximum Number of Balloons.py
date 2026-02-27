b4b022512fc1c1e831f8a7366b1e2dc633432540
Rishav Bhurtel <rishavbhurtel@Rishavs-M2-MacBook-Air.local>
2026-02-27 00:19:07 -0500

    hashing/counting

---
+++ Maximum Number of Balloons.py
@@ -0,0 +1,33 @@
+"""
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+
+Input: text = "leetcode"
+Output: 0
+"""
+
+from collections import Counter
+
+
+def maxNumberOfBalloons(text):
+    b = text.count("b")
+    a = text.count("a")
+    l = text.count("l") // 2
+    o = text.count("o") // 2
+    n = text.count("n")
+    return min(b, a, l, o, n)
+
+
+text = "loonbalxballpoon"
+yo = maxNumberOfBalloons(text)
+print(yo)
