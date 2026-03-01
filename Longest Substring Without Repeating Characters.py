dbeec6da90aded27ef52c78186513270f281cf66
Rishav Bhurtel <rishavbhurtel@Rishavs-M2-MacBook-Air.local>
2026-03-01 18:12:02 -0500

    linked list

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -35,6 +35,6 @@ def lengthOfLongestSubstring(s):
     return ans
 
 
-s = "au"
+s = "dvdf"
 yo = lengthOfLongestSubstring(s)
 print(yo)
---
+++ Middle of the Linked List.py
@@ -0,0 +1,55 @@
+"""
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        result = []
+        curr = self
+        while curr:
+            result.append(str(curr.val))
+            curr = curr.next
+        return str(result)
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+
+
+def build_linked_list(values):
+    dummy = ListNode(0)
+    curr = dummy
+    for v in values:
+        curr.next = ListNode(v)
+        curr = curr.next
+    return dummy.next
+
+
+head = build_linked_list([1, 2, 3, 4, 5])
+ans = Solution().middleNode(head)
+print(ans)
